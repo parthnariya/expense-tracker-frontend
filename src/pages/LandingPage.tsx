989762e53993336096ddb90ae8f7594a103955be
Parth Nariya <parthknariya1919@gmail.com>
2025-06-28 15:42:17 +0530

    feat: footer for landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,10 @@
-import { Container } from '@mui/material';
+import { Container, Stack, Typography } from '@mui/material';
 
 import BenefitSection from '@/components/landingpage/BenefitSection';
 import FeatureSection from '@/components/landingpage/FeatureSection';
 import HeroSection from '@/components/landingpage/HeroSection';
 import StateSection from '@/components/landingpage/StateSection';
+import AppLogo from '@/components/ui/AppLogo';
 
 const LandingPage = () => {
   return (
@@ -15,6 +16,38 @@ const LandingPage = () => {
       <StateSection />
 
       <BenefitSection />
+
+      <Container
+        sx={{
+          py: {
+            xs: 8,
+            lg: 12,
+          },
+          px: 0,
+          width: '100%',
+        }}
+        component="section"
+        maxWidth={false}
+      >
+        <Container
+          sx={{ px: { xs: 2, sm: 3, lg: 4 }, mx: 'auto' }}
+          maxWidth="lg"
+        >
+          <Stack alignItems="center" justifyContent="center">
+            <AppLogo />
+
+            <Typography
+              maxWidth="sm"
+              color="grey.600"
+              textAlign="center"
+              my={3}
+            >
+              Take control of your financial future with our comprehensive
+              personal finance management tool.
+            </Typography>
+          </Stack>
+        </Container>
+      </Container>
     </Container>
   );
 };
