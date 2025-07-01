48539d0db5465cb19fa55b9258a04fce523957ad
Parth Nariya <parthknariya1919@gmail.com>
2025-07-01 13:10:47 +0530

    feat: add custom useAppDispatch hook for improved type safety in dispatching actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 
 import spacesReducer from './slices/spaces/spacesSlice';
 import transactionsReducer from './slices/transactions/transactionsSlice';
@@ -12,3 +13,5 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
